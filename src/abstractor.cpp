da5f028ebfc13af5bec4d9db00cfd66575c5e184
Akin Elden <akin.elden@boun.edu.tr>
2022-01-11 01:20:10 +0300

    Thread operations are extended. Query parameter became const vector

--- src/abstractor.cpp
+++ src/abstractor.cpp
@@ -11,6 +11,7 @@
 #include <pthread.h>
 #include <vector>
 #include <queue>
+#include <unordered_set>
 #include <shared_mutex>
 
 using namespace std;
@@ -41,15 +42,18 @@ struct thread_parameters
     char name;
     int nResult;
     const string outputFile;
-    safe_data<vector<string>> *query;
+    const vector<string> query;
     safe_data<queue<string>> *abstracts;
-    safe_data<vector<result>> *finalResults;
+    safe_data<vector<result>> *globalResults;
 };
 
 input_parameters get_input_params(string &inputFile);
-pthread_t *create_start_thread(char name, const string &outputFile, int nResult, safe_data<vector<string>> *query, safe_data<queue<string>> *abstracts, safe_data<vector<result>> *finalResults);
-void *process_abstracts(thread_parameters *params) { cout << "Thread " << params->name << endl; return nullptr; };
-// bool compare_write_results(vector<result> &results, const string &outputFile);
+pthread_t *create_start_thread(char name, const string &outputFile, int nResult, const vector<string>& query, safe_data<queue<string>> *abstracts, safe_data<vector<result>> *globalResults);
+void *thread_process(thread_parameters *params);
+result process_abstract(const char name, const string &outputFile, const string &abstract, const vector<string>& query);
+double jaccard_score(const string &abstractText, const vector<string> &query);
+string get_summary(const string &abstractText, const vector<string> &query);
+int insert_result(result res, vector<result> *results, int nResult, int startIndex = 0);
 
 int main(int argc, char *argv[])
 {
@@ -66,24 +70,18 @@ int main(int argc, char *argv[])
 
     auto params = get_input_params(inputFile);
 
-    safe_data<vector<string>> query;
-    {
-        unique_lock lock(query._mutex);
-        query.data = params.query;
-    }
-
     safe_data<queue<string>> abstracts;
     {
         unique_lock lock(abstracts._mutex);
         abstracts.data = params.abstracts;
     }
 
-    safe_data<vector<result>> finalResults;
+    safe_data<vector<result>> globalResults;
     {
-        unique_lock lock(finalResults._mutex);
+        unique_lock lock(globalResults._mutex);
         vector<result> *results = new vector<result>();
         results->reserve(params.nResult * 2);
-        finalResults.data = results;
+        globalResults.data = results;
     }
 
     vector<pthread_t *> threads;
@@ -91,7 +89,7 @@ int main(int argc, char *argv[])
     char letter = 'A';
     for (int i = 0; i < params.nThread; i++)
     {
-        pthread_t *pThread = create_start_thread(letter + i, outputFile, params.nResult, &query, &abstracts, &finalResults);
+        pthread_t *pThread = create_start_thread(letter + i, outputFile, params.nResult, *params.query, &abstracts, &globalResults);
         if (!pThread)
         {
             cout << "Couldn't create thread : " << letter + i << endl;
@@ -164,12 +162,83 @@ input_parameters get_input_params(string &inputFile)
     return params;
 }
 
-pthread_t *create_start_thread(char name, const string &outputFile, int nResult, safe_data<vector<string>> *query, safe_data<queue<string>> *abstracts, safe_data<vector<result>> *finalResults)
+pthread_t *create_start_thread(char name, const string &outputFile, int nResult, const vector<string> &query, safe_data<queue<string>> *abstracts, safe_data<vector<result>> *globalResults)
 {
     pthread_t *thread = new pthread_t();
-    thread_parameters *tParams = new thread_parameters{name, nResult, outputFile, query, abstracts, finalResults};
-    int status = pthread_create(thread, NULL, (void *(*)(void *))process_abstracts, (void *)tParams);
+    thread_parameters *tParams = new thread_parameters{name, nResult, outputFile, query, abstracts, globalResults};
+    int status = pthread_create(thread, NULL, (void *(*)(void *))thread_process, (void *)tParams);
     if (status != 0)
         return nullptr;
     return thread;
 }
+
+void *thread_process(thread_parameters *params)
+{
+    // extract parameters
+    int nResult = params->nResult;
+    auto abstracts = params->abstracts;
+    auto globalResults = params->globalResults;
+
+    // thread local results
+    vector<result> localResults;
+    localResults.reserve(nResult * 2);
+
+    // take an abstract and process it
+    while (true)
+    {
+        string abstract;
+        {
+            // acquire write lock and get the first abstract
+            unique_lock lock(abstracts->_mutex);
+            // break the loop if no abstract left in the queue
+            if (abstracts->data->size() == 0)
+                break;
+            abstract = abstracts->data->front();
+            abstracts->data->pop();
+        }
+        result res = process_abstract(params->name, params->outputFile, abstract, params->query);
+        insert_result(res, &localResults, nResult);
+    }
+
+    // try to insert localResults to globalResults by comparing their scores
+    {
+        unique_lock lock(globalResults->_mutex);
+        int index = 0;
+        for (auto res : localResults)
+        {
+            index = insert_result(res, globalResults->data, nResult, index);
+            // if index is equal to nResult, it means the objects score is less than every element in the array
+            // so don't process remaining items since their score is lower than the current one
+            if (index == nResult)
+                break;
+        }
+    }
+
+    return nullptr;
+};
+
+result process_abstract(const char name, const string &outputFile, const string &abstract, const vector<string> &query)
+{
+    {
+        ofstream file(outputFile, ios::app);
+        file << "Thread " << name << " is calculating " << abstract << endl;
+        file.close();
+    }
+
+    string text = "";
+    {
+        ifstream file(abstract);
+        string line;
+        while (getline(file, line))
+            text += (line + " ");
+        file.close();
+    }
+    
+    double score = jaccard_score(text, query);
+    string summary = get_summary(text, query);
+    return { abstract, score, summary };
+}
+
+double jaccard_score(const string &abstractText, const vector<string> &query) {return 0.0;};
+string get_summary(const string &abstractText, const vector<string> &query) {return "";};
+int insert_result(result res, vector<result> *results, int nResult, int startIndex) {return 0;};
