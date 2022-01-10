dd8b55b544b3ec34c00e1dc052eb50a078d0d09c
Akin Elden <akin.elden@boun.edu.tr>
2022-01-10 23:39:54 +0300

    input params functions are implemented

--- src/abstractor.cpp
+++ src/abstractor.cpp
@@ -6,6 +6,7 @@
 
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
 #include <pthread.h>
 #include <vector>
@@ -17,24 +18,15 @@ using namespace std;
 template <typename T>
 struct safe_data
 {
-    mutable shared_mutex mutex_;
-    T data;
+    shared_mutex _mutex;
+    T *data;
 };
 
 struct input_parameters
 {
     int nThread, nAbstract, nResult;
-    safe_data<vector<string>> query;
-    safe_data<queue<string>> abstracts;
-};
-
-struct thread_parameters
-{
-    char name;
-    int nResult;
-    string outputFile;
-    safe_data<vector<string>> query;
-    safe_data<queue<string>> abstracts;
+    vector<string> *query;
+    queue<string> *abstracts;
 };
 
 struct result
@@ -44,18 +36,28 @@ struct result
     string summary;
 };
 
-vector<result>* process_abstracts(thread_parameters* params);
-input_parameters get_input_params(string& inputFile);
-pthread_t* create_start_thread(char name, string& outputFile, input_parameters inputParams);
-bool compare_write_results(vector<result> results, string& outputFile);
+struct thread_parameters
+{
+    char name;
+    int nResult;
+    const string outputFile;
+    safe_data<vector<string>> *query;
+    safe_data<queue<string>> *abstracts;
+    safe_data<vector<result>> *finalResults;
+};
+
+input_parameters get_input_params(string &inputFile);
+pthread_t *create_start_thread(char name, const string &outputFile, int nResult, safe_data<vector<string>> *query, safe_data<queue<string>> *abstracts, safe_data<vector<result>> *finalResults);
+void *process_abstracts(thread_parameters *params) { cout << "Thread " << params->name << endl; return nullptr; };
+// bool compare_write_results(vector<result> &results, const string &outputFile);
 
 int main(int argc, char *argv[])
 {
     if (argc < 3)
-	{
-		printf("input and output file name arguments are required.");
-		return 1;
-	}
+    {
+        cout << "input and output file name arguments are required." << endl;
+        return 1;
+    }
 
     // the path of the input file
     string inputFile = string(argv[1]);
@@ -64,44 +66,109 @@ int main(int argc, char *argv[])
 
     auto params = get_input_params(inputFile);
 
-    vector<pthread_t*> threads;
+    safe_data<vector<string>> query;
+    {
+        unique_lock lock(query._mutex);
+        query.data = params.query;
+    }
+
+    safe_data<queue<string>> abstracts;
+    {
+        unique_lock lock(abstracts._mutex);
+        abstracts.data = params.abstracts;
+    }
+
+    safe_data<vector<result>> finalResults;
+    {
+        unique_lock lock(finalResults._mutex);
+        vector<result> *results = new vector<result>();
+        results->reserve(params.nResult * 2);
+        finalResults.data = results;
+    }
+
+    vector<pthread_t *> threads;
     threads.reserve(params.nThread);
     char letter = 'A';
-    for (int i=0; i<params.nThread; i++)
+    for (int i = 0; i < params.nThread; i++)
     {
-        pthread_t* pThread = create_start_thread(letter+i, outputFile, params);
+        pthread_t *pThread = create_start_thread(letter + i, outputFile, params.nResult, &query, &abstracts, &finalResults);
         if (!pThread)
         {
-            printf("Couldn't create thread %s", letter+i);
+            cout << "Couldn't create thread : " << letter + i << endl;
             exit(1);
         }
         else
             threads.push_back(pThread);
     }
 
-    vector<result> results;
-    results.reserve(params.nThread * params.nResult);
-
     for (auto pThread : threads)
     {
-        vector<result>* res;
-        pthread_join(*pThread, (void**)&res);
-        for (auto r : *res)
-            results.push_back(r);
+        pthread_join(*pThread, NULL);
     }
 
-    bool status = compare_write_results(results, outputFile);
-    if (status)
-        return 0;
-    else
-        return 1;
+    // bool status = compare_write_results(results, outputFile);
+    // if (status)
+    //     return 0;
+    // else
+    //     return 1;
+    return 0;
 }
 
-pthread_t* create_start_thread(char name, string& outputFile, input_parameters inputParams)
+vector<string> *tokenize_string(const string &line, const string &delim)
 {
-    pthread_t* thread = new pthread_t();
-    thread_parameters* tParams = new thread_parameters{ name, inputParams.nResult, outputFile, inputParams.query, inputParams.abstracts};
-    int status = pthread_create(thread, nullptr, (void* (*)(void*))process_abstracts, (void*)tParams);
+    string cp(line);
+    auto tokens = new vector<string>();
+    tokens->reserve(100);
+    size_t index;
+    do
+    {
+        index = cp.find(delim);
+        // to prevent cases of double space etc.
+        if (index > 0)
+            tokens->push_back(cp.substr(0, index));
+        cp.erase(0, index + delim.size());
+    } while (index != string::npos);
+
+    return tokens;
+}
+
+input_parameters get_input_params(string &inputFile)
+{
+    ifstream file(inputFile);
+    if (!file)
+    {
+        cout << "Cannot open the file : " << inputFile << endl;
+        exit(1);
+    }
+
+    // first line, integer parameters
+    string line;
+    getline(file, line);
+    stringstream ss(line);
+    int T, A, N;
+    ss >> T >> A >> N;
+
+    // second line, query
+    getline(file, line);
+    vector<string> *pQuery = tokenize_string(line, " ");
+
+    queue<string> *pAbstracts = new queue<string>();
+    // read following A number of lines
+    int i = 0;
+    while (getline(file, line) && i++ <= A)
+    {
+        pAbstracts->push(line);
+    }
+
+    input_parameters params = {T, A, N, pQuery, pAbstracts};
+    return params;
+}
+
+pthread_t *create_start_thread(char name, const string &outputFile, int nResult, safe_data<vector<string>> *query, safe_data<queue<string>> *abstracts, safe_data<vector<result>> *finalResults)
+{
+    pthread_t *thread = new pthread_t();
+    thread_parameters *tParams = new thread_parameters{name, nResult, outputFile, query, abstracts, finalResults};
+    int status = pthread_create(thread, NULL, (void *(*)(void *))process_abstracts, (void *)tParams);
     if (status != 0)
         return nullptr;
     return thread;
